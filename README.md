e0a87d6bc32e9b381faf5b6d9763a48f74432ce4
Wyvern676 <johnsmith.tardis.bbc@gmail.com>
2024-12-14 12:50:22 -0600

    juypter replaces everything

---
+++ README.md
@@ -0,0 +1,4 @@
+Run jupyer lab \
+open the gait.ipynb file \
+paste API keys \
+run
--- main.py
+++
@@ -1,8 +0,0 @@
-from app.gameLogic import PokerGame
-
-def main():
-    game = PokerGame(100)
-    game.play()
-
-if __name__ == "__main__":
-    main()
